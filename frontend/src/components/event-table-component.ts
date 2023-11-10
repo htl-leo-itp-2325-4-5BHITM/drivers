3372de2bc572bfb5875bad3ad84abeffdfc79a5a
janinewenninger <janine.wenninger07@gmail.com>
2023-11-10 09:37:17 +0100

    first steps for saving in csv

---
+++ frontend/src/components/event-table-component.ts
@@ -0,0 +1,8 @@
+/*import {Event} from "../../model/model"
+
+class EventTableComponent extends HTMLElement {
+    connectedCallback() {
+        console.log("EventTable loaded")
+    }
+}
+customElements.define("event-table", EventTableComponent)*/
