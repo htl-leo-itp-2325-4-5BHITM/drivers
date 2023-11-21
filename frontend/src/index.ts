1cbc1082395bbe614c89ced0993e43632a746074
janinewenninger <janine.wenninger07@gmail.com>
2023-11-21 21:52:48 +0100

    event Listeners

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -44,8 +44,48 @@ class AdminTS {
                 <input type="text" id="fahrer" name="fahrer"><br>
             </div>
             <input type="submit" id="submit" name="senden" onclick="showList()">
-        </form>`;
+        </form>
+        <button id="backToTableBtn">back to rides</button>`;
+    }
+  }
+  // start the app
+ 
+
+
+  class backToTable {
+    constructor() {
+      let btn = document.getElementById("backToTableBtn");
+      btn.addEventListener("click", (e:Event) => this.showTable());
+    }
+    showTable(){
+       // button click handler
+       document.getElementById("ride-finder-tab").innerHTML = `
+       <h1 id="headline-table">Available Rides</h1>
+       <table id="table">
+           <thead class="table-head">
+               <tr>
+                   <th>Date</th>
+                   <th>Departure Time</th>
+                   <th>Place of Departure</th>
+                   <th>Place of Arrival</th>
+                   <th>Available seats</th>
+                   <th>Driver</th>
+               </tr>
+           </thead>
+           <tbody>
+               <tr id="test">
+                   <td>Date</td>
+                   <td>Departure Time</td>
+                   <td>Place of Departure</td>
+                   <td>Place of Arrival</td>
+                   <td>Available seats</td>
+                   <td>Driver</td>
+               </tr>
+           </tbody>
+       </table>
+       <button id="backToTableBtn">back to rides</button>`;
     }
   }
   // start the app
   new AdminTS();
+  new backToTable();
