14d21b88fe579c82aef0f16d56377f3ba5c2b60a
romanaSchned1 <93990466+romanaSchned1@users.noreply.github.com>
2023-11-22 19:09:07 +0100

    style change ride

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -18,17 +18,17 @@ class AdminTS {
        // button click handler
        document.getElementById("ride-finder-tab").innerHTML = `
  
-       <h1 id="form_headline">Change Ride</h1>
-       <form id="form_head">
-           <div class="table-input" id="form_label">
-               <label for="datum">Date:</label>
-               <label for="abfzeit">Departure Time:</label>
-               <label for="abfort">Place of Departure:</label>
-               <label for="ankort">Place of Arrival:</label>
-               <label for="fplatz">Available seats:</label>
-               <label for="fahrer">Driver:</label>
+       <h1 id="form_headline_change_ride">Change Ride</h1>
+       <form id="form_head_change_ride">
+           <div class="table-input-change-ride">
+               <label for="datum" class="label_change_ride">Date:</label>
+               <label for="abfzeit" class="label_change_ride">Departure Time:</label>
+               <label for="abfort" class="label_change_ride">Place of Departure:</label>
+               <label for="ankort" class="label_change_ride">Place of Arrival:</label>
+               <label for="fplatz" class="label_change_ride">Available seats:</label>
+               <label for="fahrer" class="label_change_ride">Driver:</label>
            </div>
-           <div class="table-input">
+           <div class="table-input-change-ride">
                <input type="date" id="datum" name="datum">
                <input type="time" id="abfzeit" name="abfzeit">
                <input type="text" id="abfort" name="abfort">
