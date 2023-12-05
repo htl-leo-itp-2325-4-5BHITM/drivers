a83b1490c9fae6799f08d2f9a5c4f1e965f7c30c
“[andrei <tnsteve123@gmail.com>
2023-12-05 07:27:41 +0100

    animations changed

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,3 +1,14 @@
-ScrollReveal().reveal('#ride-input-tab', { delay: 300 });
-ScrollReveal().reveal('#reason', { delay: 300 });
+//Home section
+ScrollReveal().reveal('#info-box-headline', { delay: 100 });
+ScrollReveal().reveal('#usefull-buttons', { delay: 300 });
+
+//Reason section
+ScrollReveal().reveal('#reason-content', { delay: 400 });
+ScrollReveal().reveal('#reason-image', { delay: 100 });
+
+//Register ride section
+
+ScrollReveal().reveal('#register-ride-content', { delay: 100 });
+ScrollReveal().reveal('#register-ride-forum input', { delay: 300, interval: 100 });
+ScrollReveal().reveal('#register-ride-forum label', { delay: 300, interval: 100 });
 //i werd eh mehr hinzufügen und anpassen...wollte nur was ausprobieren
