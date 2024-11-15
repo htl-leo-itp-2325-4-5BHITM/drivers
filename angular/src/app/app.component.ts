22a3ad66f9e4446508a20b0b51e5534a458d1d6c
Andrei Brehar <tnsteve123@gmail.com>
2024-11-15 13:17:42 +0100

    login component

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
-import {TableComponent} from './table/table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, AboutComponent, TableComponent],
+  imports: [RouterOutlet, HomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angular/src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  login: FormGroup = new FormGroup({
+    'username': new FormControl()
+  })
+
+  loginFunction(){
+
+  }
+
+
+
+}
