b52841e3245f8bbe806443be649d121c77d49748
Andrei Brehar <tnsteve123@gmail.com>
2024-11-19 11:22:03 +0100

    signup component

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -11,13 +11,17 @@ import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username?: string;
+  password?: string;
+
   login: FormGroup = new FormGroup({
     'username': new FormControl(),
     'password': new FormControl()
   })
 
   loginFunction(){
-
+    this.username=this.login.get('username')?.value;
+    this.password=this.login.get('password')?.value;
   }
 
 
