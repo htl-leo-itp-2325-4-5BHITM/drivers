edebd89ed5b5a73e278624a27aa3f9754b476607
Andrei Brehar <tnsteve123@gmail.com>
2024-11-28 19:23:36 +0100

    ride registration

--- angular/src/app/ride-register-view/ride-register-view.component.ts
+++ angular/src/app/ride-register-view/ride-register-view.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from '../navbar/navbar.component';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-ride-register-view',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './ride-register-view.component.html',
   styleUrl: './ride-register-view.component.css'
 })
 export class RideRegisterViewComponent {
+  registerRide: FormGroup = new FormGroup({
+  })
 
+  registerRideFunction() {
+
+  }
 }
