b0c024a7f6f8e70fe04c8965d0ee5a610ae5d798
Andrei Brehar <tnsteve123@gmail.com>
2024-11-28 15:33:14 +0100

    structure for rides

--- angular/src/app/about/about.component.ts
+++ angular/src/app/about/about.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NavbarComponent} from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
