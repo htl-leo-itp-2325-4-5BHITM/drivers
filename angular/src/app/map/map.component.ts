302f7c93f2b2013cc7da509fb627542d9099a45d
janinewenninger <janine.wenninger07@gmail.com>
2024-12-05 15:18:39 +0100

    #92

---
+++ angular/src/app/map/map.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
