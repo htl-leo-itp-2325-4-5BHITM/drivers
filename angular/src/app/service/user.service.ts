d05bff1ea4f69f64437c62b0dad376fc9c9c2c24
Natalie Schmitzberger :0 <91959094+schnurgel@users.noreply.github.com>
2024-12-05 13:59:32 +0100

    login with sessionstorage

--- angular/src/app/service/user.service.ts
+++ angular/src/app/service/user.service.ts
@@ -8,7 +8,7 @@ import {User} from '../model/user.model';
 })
 export class UserService {
 
-  private url = "http://localhost:8080/api/drivus/users"
+  private url = "http://localhost:8080/api/drivus"
 
   constructor(private http: HttpClient) { }
 
@@ -16,11 +16,27 @@ export class UserService {
     console.log("in createUser")
     console.log(user)
 
-    this.http.post<User>(this.url + '/postUser', user).subscribe(user => {
+    this.http.post<User>(this.url + '/users/postUser', user).subscribe(user => {
       console.log('Updated user:', user);
     });
 
     //return this.http.post(this.url + "postUser/", user)
+  }
+
+  getUserDetails(password?: String, username?: string){
+    //const body = { username: username, password: password };
+
+    this.http.post<User>(this.url + '/getUserByUsername', username).subscribe((response) => {
+        let user: User = response;
+        sessionStorage.setItem('firstname',user.firstName);
+        sessionStorage.setItem('lastname',user.lastName);
+        sessionStorage.setItem('email',user.emailAddress);
+        sessionStorage.setItem('phoneNr',user.phoneNr);
+      },
+      (error) => {
+        console.error('Fehler bei der Login-Anfrage:', error);
+      }
+    );
   }
 
   loginValid(password?: String, username?: string ){
@@ -29,9 +45,11 @@ export class UserService {
 
     const body = { username: username, password: password };
 
-    this.http.post<boolean>(this.url + '/postLogIn', body).subscribe((isValid: boolean) => {
+    this.http.post<boolean>(this.url + '/users/postLogIn', body).subscribe((isValid: boolean) => {
         if (isValid) {
           console.log('Login erfolgreich!!!');
+          sessionStorage.setItem('isloged','true');
+          this.getUserDetails(password,username);
         } else {
           console.log('Login fehlgeschlagen!!!');
         }
