bdbdce106ba7986ecbf2413ac3eb3bbe9f9f3250
Andrei Brehar <tnsteve123@gmail.com>
2024-12-11 20:30:00 +0100

    added account component

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import {SignupComponent} from './signup/signup.component';
 import {RideViewComponent} from './ride-view/ride-view.component';
 import {RideRegisterViewComponent} from './ride-register-view/ride-register-view.component';
 import {AboutComponent} from './about/about.component';
+import {AccountComponent} from './account/account.component';
 
 export const routes: Routes = [
   {path: '', component:HomeComponent},
@@ -12,5 +13,6 @@ export const routes: Routes = [
   {path: 'signup', component:SignupComponent},
   {path:'rides', component:RideViewComponent},
   {path: 'register', component:RideRegisterViewComponent},
-  {path: 'about', component:AboutComponent}
+  {path: 'about', component:AboutComponent},
+  {path: 'account', component: AccountComponent}
 ];
